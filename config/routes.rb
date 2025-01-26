5ddc8b723ea639a2d69c77f43f650e84a4a5092d
RYOTASUDA <sdrt0913@gmail.com>
2025-01-26 13:49:15 +0900

    healthcheck

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     delete '/logout', to: 'sessions#destroy'
+    get '/healthcheck', to: 'healthcheck#show'
 
     resource :users, only: %i[show]
     resources :categories, only: %i[index create edit update destroy]

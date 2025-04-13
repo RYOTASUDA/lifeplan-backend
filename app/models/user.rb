5dd170ec2a41098ffc0c0a4654e129ae96fffd16
RYOTASUDA <sdrt0913@gmail.com>
2025-04-13 17:04:47 +0900

    fix user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
   has_many :categories, dependent: :destroy
   has_many :plans, dependent: :destroy
 
-  GMAIL_REGEXP = %r{\A[a-zA-Z0-9.!\#$%&'*+/=?^_`{|}~-]+@gmail\.com\z}
+  MAIL_REGEXP = %r{\A[a-zA-Z0-9.!\#$%&'*+/=?^_`{|}~-]+@.+\z}
 
   validates :name, presence: true
   validates :email,
             presence: true,
             uniqueness: { case_sensitive: false },
-            format: { with: GMAIL_REGEXP }
+            format: { with: MAIL_REGEXP }
 
   class << self
     def find_or_initialize_by_email(email)
